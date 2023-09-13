cef9d76c08371edcb2f749709330ba8130e48e4d
rkadlick <27776470+rkadlick@users.noreply.github.com>
2023-09-13 19:33:08 -0400

    email post success

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,62 @@
+import { signUpSchema } from "@/lib/formSchema";
+import { NextResponse } from "next/server";
+import { mailOptions, transporter } from "../../config/nodemailer";
+
+const CONTACT_MESSAGE_FIELDS = {
+  name: "Name",
+  email: "Email",
+  message: "Message",
+};
+
+const generateEmailContent = (dataObj) => {
+  const data = dataObj.data;
+  const stringData = Object.entries(data).reduce(
+    (str, [key, val]) =>
+      (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
+    ""
+  );
+  const htmlData = Object.entries(data).reduce((str, [key, val]) => {
+    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
+  }, "");
+
+  return {
+    text: stringData,
+    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+  };
+};
+
+export async function POST(request: Request) {
+  const body: unknown = await request.json();
+
+  const result = signUpSchema.safeParse(body);
+  // check out Zod's .flatten() method for an easier way to process errors
+  let zodErrors = {};
+  if (!result.success) {
+    result.error.issues.forEach((issue) => {
+      zodErrors = { ...zodErrors, [issue.path[0]]: issue.message };
+    });
+  }
+
+  if(Object.keys(zodErrors).length > 0){
+    return NextResponse.json({
+      errors: zodErrors
+    })
+  }
+  try {
+    const ans = await transporter.sendMail({
+      ...mailOptions,
+      ...generateEmailContent(result),
+      subject: "Portfolio Contact"
+    });
+    return NextResponse.json({ 
+      success: true,
+      status: 200 
+    });
+  } catch (err : any) {
+    // console.log(err);
+    return NextResponse.json({ 
+      message: err.message 
+    });
+  }
+
+}
--- src/app/components/contact/ContactForm.tsx
+++ src/app/components/contact/ContactForm.tsx
@@ -6,57 +6,125 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import styles from "./contactForm.module.css";
 import { heading, text } from "@/styles/fonts";
 import { TSignUpSchema, signUpSchema } from "@/lib/formSchema";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors, isSubmitting, isSubmitSuccessful },
     reset,
+    setError,
   } = useForm<TSignUpSchema>({
-	resolver: zodResolver(signUpSchema)
+    resolver: zodResolver(signUpSchema),
   });
 
-  const onSubmit = async(data: TSignUpSchema) => {
-	reset();
-  }
+  const onSubmit = async (data: TSignUpSchema) => {
+    // console.log(JSON.stringify(data));
+    const response = await fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    console.log(response);
+
+    if (!response.ok) {
+      // response status is not 2xx
+      alert("Submitting form failed!");
+      return;
+    }
+    const responseData = await response.json();
+    // console.log(responseData);
+    if (responseData.errors) {
+      const errors = responseData.errors;
+
+      if (errors.name) {
+        setError("name", {
+          type: "server",
+          message: errors.name,
+        });
+      } else if (errors.email) {
+        setError("email", {
+          type: "server",
+          message: errors.email,
+        });
+      } else if (errors.message) {
+        setError("message", {
+          type: "server",
+          message: errors.message,
+        });
+      } else {
+        alert("Something went wrong!");
+      }
+    }
+
+    // reset();
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.contactForm}>
       <input
         {...register("name")}
+		id="name"
         className={text.className + " " + styles.contactName}
         type="text"
         placeholder="Name"
+		disabled={isSubmitSuccessful || isSubmitting}
       />
-	  {errors.name && (
-		<p className={styles.error}>{`${errors.name.message}`}</p>
-	  )}
+      {errors.name && (
+        <p className={styles.error}>{`${errors.name.message}`}</p>
+      )}
       <input
         {...register("email")}
+		id="email"
         className={text.className + " " + styles.contactEmail}
         type="email"
         placeholder="Email"
+		disabled={isSubmitSuccessful || isSubmitting}
       />
-	{errors.email && (
-		<p className={styles.error}>{`${errors.email.message}`}</p>
-	  )}
+      {errors.email && (
+        <p className={styles.error}>{`${errors.email.message}`}</p>
+      )}
       <textarea
         {...register("message")}
+		id="message"
         className={text.className + " " + styles.contactMessage}
         rows={4}
         placeholder="Message..."
+		disabled={isSubmitSuccessful || isSubmitting}
       />
-	  	  {errors.message && (
-		<p className={styles.error}>{`${errors.message.message}`}</p>
-	  )}
-      <button
-        className={heading.className + " " + styles.contactSubmit}
-        type="submit"
-		disabled = {isSubmitting}
-      >
-		Send
-	  </button>
+      {errors.message && (
+        <p className={styles.error}>{`${errors.message.message}`}</p>
+      )}
+      {isSubmitting ? (
+        <button
+          className={heading.className + " " + styles.contactSending}
+          type="submit"
+          disabled
+        >
+          Sending...
+        </button>
+      ) : isSubmitSuccessful ? (
+        <button
+          className={heading.className + " " + styles.contactSent}
+          type="submit"
+          disabled
+        >
+          Message Sent <FontAwesomeIcon icon={faCheck} />
+        </button>
+      ) : (
+        <button
+          className={heading.className + " " + styles.contactSubmit}
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Send
+        </button>
+      )}
     </form>
   );
 }
