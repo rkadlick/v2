010a67d6c787b8fca1b40d1f160f4a337ef238ce
rkadlick <27776470+rkadlick@users.noreply.github.com>
2023-10-02 16:36:12 -0400

    contact text change

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -58,9 +58,8 @@ function Contact() {
         </h3>
         <div className={styles.formContainer}>
           <p className={styles.contactText}>
-            Although I’m not currently looking for any new opportunities, my
-            inbox is always open. Whether you have a question or just want to
-            say hi, I’ll try my best to get back to you!
+          Got a question or proposal, or just want
+          to say hello? Please reach out.
           </p>
           <ContactForm />
         </div>
